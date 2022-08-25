aaf2e04c4bda6e803f8f68cab3977604e8196ac0
18Kailash <sumer.ki359@gmail.com>
2022-08-25 21:17:26 +0530

    day3 task complete

--- search.js
+++ search.js
@@ -1,1 +1,13 @@
 //
+let doc=document.querySelector("#fotter")
+doc.addEventListener("click",function(){
+    window.location.href="https:www.naukri.com/advanced-search"
+})
+
+
+let logo=document.querySelector("#playLOgo")
+doc.addEventListener("click",function(){
+    window.location.href="https://play.google.com/store/apps/details?id=naukriApp.appModules.login&hl=en&utm_source=naukri&utm_medium=footer"
+})
+  
+  
