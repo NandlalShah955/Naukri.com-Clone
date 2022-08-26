a4cffd9ebdf83da6f5687f871adc1c7cd634ebf1
18Kailash <sumer.ki359@gmail.com>
2022-08-26 20:50:32 +0530

    day3 task complete

--- search.js
+++ search.js
@@ -10,4 +10,28 @@ doc.addEventListener("click",function(){
     window.location.href="https://play.google.com/store/apps/details?id=naukriApp.appModules.login&hl=en&utm_source=naukri&utm_medium=footer"
 })
   
+
+
+let middle=document.querySelector("#middlebtn")
+middle.addEventListener("click",function(){
+    window.location.href="https://www.naukri.com/job-listings-urgent-hiring-for-java-professional-iris-software-iris-software-inc-noida-pune-chennai-3-to-8-years-190822001305?src=jobsearchDesk&sid=16614865167322828&xp=1&px=1"
+})
   
+let Excelentdata=document.querySelector("#excelentBtn")
+Excelentdata.addEventListener("click",function(){
+    window.location.href="https://www.naukri.com/job-listings-excellent-opportunity-java-developer-ibm-india-pvt-limited-bangalore-bengaluru-2-to-5-years-250822004074?src=jobsearchDesk&sid=16614865167322828&xp=2&px=1"
+})
+
+
+
+
+
+let leftsideSide=document.querySelector("#leftside")
+leftsideSide.addEventListener("click",function(){
+    window.location.href="https://www.naukri.com/jobs-in-india?clusters=experience,functionalAreaGid&experience=0&functionAreaIdGid=2&wfhType=1"
+})
+
+
+
+
+
