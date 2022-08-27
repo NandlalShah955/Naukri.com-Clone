83566bbe801e7d98043d23d48f19d7e46631664a
IInnffiinniittyy <tanuj813@gmail.com>
2022-08-27 18:46:33 +0530

    Final Submission

--- navbar/t_login.js
+++ navbar/t_login.js
@@ -2,7 +2,6 @@ import navbar_t from "../components/t_nabar_com.js";
 
 document.querySelector("#login_navbar").innerHTML = navbar_t();
 
-
 class user {
     constructor() {}
     login(uemail, upassword) {
