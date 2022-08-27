7f780922401af2d9e9f041363d987e537420290c
IInnffiinniittyy <tanuj813@gmail.com>
2022-08-27 18:00:29 +0530

    Submission

--- Shaikh.JobSearch/job.js
+++
@@ -1,9 +0,0 @@
-
-let arr= [
-    {work :"Laravel Web Developer - Work From Home Now",
-     comapny :" Keyideas Infotech4.7 ★(40 Reviews)" ,
-    Experience :" Experience 1-4 years",   },
-
-
-    
-]
